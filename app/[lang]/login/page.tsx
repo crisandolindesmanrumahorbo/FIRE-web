d223587058ef2435d7d5d6b559afbee80852d8d9
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-05-01 21:29:22 +0700

    add logo fire

--- app/[lang]/login/page.tsx
+++ app/[lang]/login/page.tsx
@@ -8,18 +8,21 @@ async function Login() {
     <>
       <Navbar />
       <div className="w-full min-h-screen flex justify-center items-center">
-        <div className="border border-gray-700 p-12 mx-4 rounded-lg">
+        <div className="flex xl:flex-row flex-col items-center gap-10 border border-gray-700 p-12 mx-4 rounded-lg h-full">
           <div className="w-full items-center justify-center flex mb-6">
-            <div className="relative w-[100px] h-[100px]">
+            <div className="relative sm:w-[400px] sm:h-[400px] w-[200px] h-[200px]">
               <Image
-                src="/btc.png"
-                alt="BTC Logo"
+                src="/fire.png"
+                alt="Apiiiii"
                 fill
                 className="object-contain"
               />
             </div>
           </div>
-          <FormLogin />
+
+          <div className="h-full">
+            <FormLogin />
+          </div>
         </div>
       </div>
     </>
